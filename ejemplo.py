b9b910aaacae948d38cd3dda96c6f3b3a00e5358
Sebastián Echavarría Montaña <sechavarriam@unal.edu.co>
2022-08-11 21:56:48 -0500

    Ensamble matriz estructura - Actualización a clase 11/08/2022

--- ejemplo.py
+++ ejemplo.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math #Importa la librería math de python para poder usar las funciones matemáticas usuales.
+from K_cercha import K_cerchaGlobal
+# Lo ideal es que las importaciones vayan al principio del archivo (o en un archivo aparte).
 
 # DATOS SECCIÓN
 E = 210e6      #[kPa] = 200 [GPa]
@@ -9,9 +12,9 @@ r1 = 1.0
 r2 = 1.2
 
 t0 = 0
-t1 = 30
+t1 = 90/4
 t2 = 45
-t3 = 60
+t3 = 3*90/4
 t4 = 90
 
 # Creación de arreglo de nodos en coordenadas polares. =================================================
@@ -39,7 +42,7 @@ nodosPolares = np.array([
 
 # ======================================================================================================
 
-import math #Importa la librería math de python para poder usar las funciones matemáticas usuales.
+
 
 # Creación de lista vacía dónde se almacenarán las coordenadas de los nodos en coordenadas cartesianas.
 # Lo usual es que una estructura se describa directamente en este sistema coordenado.
@@ -80,7 +83,7 @@ restricciones = np.array([
 
 # https://www.geeksforgeeks.org/flatten-a-matrix-in-python-using-numpy/
 rest_DoF = restricciones.flatten()
-#print(rest_DoF)
+print(rest_DoF)
 
 #rest_index = np.argwhere(rest_DoF)
 #print(rest_index)
@@ -96,6 +99,7 @@ rest_DoF = restricciones.flatten()
 rest_index = np.where(rest_DoF != 0)[0]
 free_index = np.where(rest_DoF == 0)[0]
 
+print(np.where(rest_DoF != 0))
 #print(rest_index)
 #print(free_index)
 
@@ -146,8 +150,6 @@ L = [] # Lista que almacenará la longitud de cada elemento
 
 K_elem = [] #Lista que almacenará las matrices globales de cada elemento.
 
-from K_cercha import K_cerchaGlobal
-# Lo ideal es que las importaciones vayan al principio del archivo (o en un archivo aparte).
 
 i = 0
 for elem in elementos:
@@ -171,12 +173,12 @@ for elem in elementos:
     L.append(math.sqrt((xj-xi)**2+(yj-yi)**2)) # Pone el valor calculado de la longitud al final de la lista.
 
     K_elem.append(K_cerchaGlobal(theta[i], E, A, L[i])) #Cálculo y almacenamiento de matriz de rigidez global por elemento.
-
+    
     i=i+1
 
 # ------- Gráfica de la malla --------------
-#from plot_Struct1D import plot_2D_Truss
-#plot_2D_Truss(nodos, elementos)
+from plot_Struct1D import plot_2D_Truss
+plot_2D_Truss(nodos, elementos)
 # ------------------------------------------
 
 ## ========================================================================================
@@ -192,7 +194,7 @@ K = np.zeros([DoF,DoF]) #Inicialización de la matriz de rigidez global de la es
 #   1. The count of the current iteration (i)
 #   2. The value of the item at the current iteration (elem)
 
-for i, elem in enumerate(elementos): 
+for e, elem in enumerate(elementos): 
 
     ni = elem[0] # índice de nodo inicial
     nj = elem[1] # Índice de nodo final
@@ -208,12 +210,19 @@ for i, elem in enumerate(elementos):
     xj = nodos[nj][0]
     yj = nodos[nj][1]
 
+    inicialI = 2*ni
+    finalI = 2*ni + 1
 
+    inicialJ = 2*nj
+    finalJ = 2*nj + 1
 
+    K[inicialI:finalI+1,inicialI:finalI+1] = K[inicialI:finalI+1,inicialI:finalI+1] +  K_elem[e][0:2,0:2]
+    K[inicialJ:finalJ+1,inicialJ:finalJ+1] += K_elem[e][2:4,2:4]
 
+    K[inicialI:finalI+1,inicialJ:finalJ+1] += K_elem[e][0:2,2:4]
+    K[inicialJ:finalJ+1,inicialI:finalI+1] += K_elem[e][2:4,0:2]
 
-
-#print(K)
+print(K)
 ## ========================================================================================
 ## ========================================================================================
 
@@ -232,20 +241,24 @@ Knn = [[K[i][j] for j in free_index] for i in free_index]
 Kaa = [[K[i][j] for j in rest_index] for i in rest_index]
 Kan = [[K[i][j] for j in rest_index] for i in free_index]
 
+
+
 Knn = np.matrix(Knn)
 Kaa = np.matrix(Kaa)
 Kan = np.matrix(Kan)
 Kna = Kan.transpose()
 
 
-#import matplotlib.pyplot as plt
-#fig, ax = plt.subplots()
-#plt.spy(K, marker='o')
-#plt.show()
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+plt.spy(K, marker='o')
+plt.show()
+
 
 # https://stackoverflow.com/questions/29102955/how-to-print-numpy-objects-without-line-breaks
-np.set_printoptions(threshold=np.inf)
-np.set_printoptions(linewidth=np.inf)
+#np.set_printoptions(threshold=np.inf)
+#np.set_printoptions(linewidth=np.inf)
 
 #print(np.round(Knn,0))
 #print(np.round(Kaa,0))
