19922c937c9d2ff47fe4871869e2919fcfbea79e
Daniela <dvallejo@unal.edu.co>
2022-08-31 21:01:12 -0500

    Archivos del portico

---
+++ Portico/k_portico.py
@@ -0,0 +1,55 @@
+
+
+import math
+import numpy as np
+
+# Definición de función
+def K_porticoGlobal(angulo, moduloE, inercia, area, longitud):
+    c = math.cos(angulo)  #lamba cos(teta)
+    s = math.sin(angulo)  #miu sin(teta)
+
+    T = np.zeros([6,6])
+    T = np.matrix(T)
+    
+
+    T[0,0] = c
+    T[0,1] = s
+    T[1,0] = -s
+    T[1,1] = c
+    T[2,2] = 1
+    T[3,3] = c
+    T[3,4] = s
+    T[4,3] = -s
+    T[4,4] = c
+    T[5,5] = 1
+
+    EAL = moduloE*area/longitud
+    EI= moduloE*inercia
+
+    k = np.zeros([6,6])
+    k = np.matrix(k)
+
+    k[0,0] = EAL
+    k[0,3] = -EAL
+    k[1,1] = 12*EI/longitud**3
+    k[1,2] = 6 * EI / longitud**2
+    k[1,4] = -12 * EI / longitud**3
+    k[1,5] = 6 * EI / longitud**2
+    k[2,1] = 6 * EI / longitud**2
+    k[2,2] = 4 * EI / longitud
+    k[2,4] = -6 * EI / longitud**2
+    k[2,5] = 2 * EI / longitud
+    k[3,0] = -EAL
+    k[3,3] = EAL
+    k[4,1] = -12 * EI / longitud**3
+    k[4,2] = -6 * EI / longitud**2
+    k[4,4] = 12 * EI / longitud**3
+    k[4,5] = -6 * EI / longitud**2
+    k[5,1] = 6 * EI / longitud**2
+    k[5,2] = 2 * EI / longitud
+    k[5,4] = -6 * EI / longitud**2
+    k[5,5] = 4 * EI / longitud
+    
+    K = np.transpose(T) * k * T
+    #print(K)
+    return K
---
+++ Portico/plot_Struct1D.py
@@ -0,0 +1,39 @@
+## Función para graficar estructura con elementos 1-dimesionales (e.g. cerchas, pórticos)
+import matplotlib.pyplot as plt
+
+def plot_2D_frame(nodos, elementos):
+    n_nodos = len(nodos)
+    n_elem  = len(elementos)
+
+    fig, ax = plt.subplots()
+    # https://stackoverflow.com/questions/34162443/why-do-many-examples-use-fig-ax-plt-subplots-in-matplotlib-pyplot-python
+
+    for elem in elementos:
+        
+        ni = elem[0] # índice de nodo inicial
+        nj = elem[1] # Índice de nodo final
+
+
+        # Extracción de coordenadas nodo inicial
+        xi = nodos[ni][0]
+        yi = nodos[ni][1]
+
+        # Extracción de coordenadas nodo final
+        xj = nodos[nj][0]
+        yj = nodos[nj][1]
+
+        ax.plot([xi, xj], [yi, yj], 'k-o') 
+        # https://matplotlib.org/2.1.2/api/_as_gen/matplotlib.pyplot.plot.html
+        
+        ax.set_aspect('equal', 'box')
+
+    plt.show()
+
+    #return 0
+
+
+
+
+
+
+
